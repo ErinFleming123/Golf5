c297feb6a688192c40f29495bc07b0917e02bf64
ErinFleming <flemingerin@hotmail.com>
2020-04-25 10:48:28 -0600

    another commit

--- Golf5.js
+++ Golf5.js
@@ -1,14 +1,22 @@
-class ScoreCard {
-  constructor(){
+// click on Fox Hollow button and all the information for that course will appear on the card
 
-  }
 
-}
 
-//input scores
+// click on Thanksgiving Point button and all the information for that course will appear on the card
 
-//add players
 
-//sum up in scores
 
-//sum up out scores
+// click on Spanish Oaks button and all the information for that course will appear on the card
+
+
+
+
+//class CourseSearch {
+  //constructor(name, id) {
+    
+  //}
+  let api = fetch("https://golf-courses-api.herokuapp.com/courses/18300")
+    .then (data => data.json().then(data => {
+        console.log(data.data.status);
+    }));
+  
